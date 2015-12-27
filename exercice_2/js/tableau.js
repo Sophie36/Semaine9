35148c9f877c92b12582584d976a239679fbedf1
sophie36 <sophielemaire361@gmail.com>
2015-12-27 19:34:15 +0000

    tableau

---
+++ exercice_2/js/tableau.js
@@ -0,0 +1,29 @@
+$liste = document.getElementById("liste");
+$texte = document.getElementById("texte");
+$ajouter = document.getElementById("ajouter");
+$annuler = document.getElementById("annuler");
+
+tableau = ["Cindy", "Florian", "Amélie", "Julien"];
+
+function afficherListe() {
+    elements = "";
+    for (i = 0; i < tableau.length; i++) {
+        elements += "<li>" + tableau[i] + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+function ajouter() {
+    tableau.push($texte.value);
+    afficherListe();
+}
+
+$ajouter.onclick = ajouter;
+
+function annuler() {
+    tableau.push($texte.value);
+    afficherListe();
+}
+
+$annuler.onclick = annuler;
+afficherListe();
