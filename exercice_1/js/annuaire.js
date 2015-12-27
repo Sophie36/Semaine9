f507e938b2c6517adacf56e4e72e1cd6d4077cd2
sophie36 <sophielemaire361@gmail.com>
2015-12-27 19:00:41 +0000

    annuaire

---
+++ exercice_1/js/annuaire.js
@@ -0,0 +1,43 @@
+$liste = document.getElementById("liste");
+$prenom = document.getElementById("prenom");
+$nom = document.getElementById("nom");
+$numero = document.getElementById("numero");
+$ajouter = document.getElementById("ajouter");
+
+contacts = [{
+    prenom: "Julien",
+    nom: "Lemaire",
+    numero: "0123456789"
+},{
+    prenom: "Cindy",
+    nom: "Lemaire",
+    numero: "0123456789"
+},{
+    prenom: "Amélie",
+    nom: "Lemaire",
+    numero: "0123456789"
+}];
+
+function texteContact(contact) {
+    return contact.prenom + " " + contact.nom + " (" + contact.numero + ")";
+}
+
+function afficherListe() {
+    elements = "";
+    for (i = 0; i < contacts.length; i++) {
+        elements += "<li>" + texteContact(contacts[i]) + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+function ajouter() {
+    contacts.push({
+        prenom: $prenom.value,
+        nom: $nom.value,
+        numero: $numero.value
+    });
+    afficherListe();
+}
+
+$ajouter.onclick = ajouter;
+afficherListe();
